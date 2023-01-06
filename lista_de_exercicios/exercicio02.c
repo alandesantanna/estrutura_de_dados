4972218ce6847c44d3b29e1086502b79e2a21ebd
Alan de Sant Anna <108240669+alandesantanna@users.noreply.github.com>
2023-01-05 21:44:20 -0800

    00122

--- lista_de_exercicios/exercicio02.c
+++ lista_de_exercicios/exercicio02.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main{
-    int i, vetA[5], vetB[5], vetC[5];
+int main(){
+    int i, vetA[25], vetB[25], vetC[25];
     
-    for( i=0; i < 5; i++){
-        printf("Digite o valor do elemento %i: ", i)
-        scanf("&d ", &vetA[i]);
+    for( i=0; i < 25; i++){
+        printf("Digite o valor do elemento %i no vetor A: ", i);
+        scanf("%i", &vetA[i]);
     }
 
-    for(i=0 ; i<5; i++){
-        printf("Digite o valor do elemento %i: ", i)
-        scanf("&d ", &vetB[i]);
+    for(i=0 ; i < 25; i++){
+        printf("Digite o valor do elemento %i no vetor B: ", i);
+        scanf("%i", &vetB[i]);
     }
 
-    for(i=0; i<5; i++){
+    for(i=0; i < 25; i++){
         vetC[i] = vetA[i] + vetB[i];
     }
-
+    
     printf("\nElementos do vetor A: \n");
 
-    for(i = 0; i<5 ; i++){
-        printf("%d ", vetA[i]);
+    for(i = 0; i<25; i++){
+        printf("%i ", vetA[i]);
     }
 
-    printf("\n\nElementos do vetor B: \n");
+    printf("\nElementos do vetor B: \n");
 
-    for(i = 0; i<5 ; i++){
+    for(i = 0; i<25; i++){
         printf("%d ", vetB[i]);
     }
 
     printf("\n\nElementos do vetor C: \n");
 
-    for(i=0; i<5; i++){
+    for(i=0; i<25; i++){
         printf("%d ", vetC[i]);
     }
+     
 
-    return 0; 
+   return 0;
 }
 
