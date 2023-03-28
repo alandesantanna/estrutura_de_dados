23db798f88349b24533fa5529fe75f98896444b8
Alan de Sant Anna <alansantanna2001@gmail.com>
2023-03-28 02:48:05 -0300

    reescrevendo algoritmos2

--- lendoVetor/LendoVetor.c
+++ lendoVetor/LendoVetor.c
@@ -4,14 +4,17 @@
 int main(){
     int i;
     int num2[10];
-
-    for(i=0; i < 10; i++){
-        printf("Digite o elemento da posicao %i: \n", i);
-        scanf("%d", &num2[i]);
+    for (i = 0; i < 10; i++){
+        printf("Digite o elemento da posicao %d: ", i);
+        scanf("%i", &num2[i]);
     }
+    
+    printf("\n\n");
 
-    for(i = 0; i<10 ; i++)
-        printf("%d ", num2[i]);
+    for (i = 0; i<10; i++)
+        printf("%i ", num2[i]);
+    
+    printf("\n\n");
 
     return 0;
 }
